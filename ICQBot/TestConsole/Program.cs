76491a30ef2cc90ab7e548ce087ddc2622b9286f
Alex Cheung <alexcheung1980@hotmail.com>
2020-04-12 04:26:09 +0800

    Use Event.Chat.ChatId

--- ICQBot/TestConsole/Program.cs
+++ ICQBot/TestConsole/Program.cs
@@ -16,9 +16,9 @@ namespace TestConsole
         private static async void Bot_NewMessage(object sender, ICQBot.Models.ICQEventArgs<ICQBot.Models.NewMessageEvent> e)
         {
 
-            await bot.SendMessageText("6410422", $"Re: {e.Event.Text}");
+            await bot.SendMessageText(e.Event.Chat.ChatId, $"Re: {e.Event.Text}");
 
-            //await bot.SendMessageText("6410422", $"Re: {e.Event.Text}", new ICQBot.Models.InlineKeyboard[] { 
+            //await bot.SendMessageText(e.Event.Chat.ChatId, $"Re: {e.Event.Text}", new ICQBot.Models.InlineKeyboard[] { 
             //    new ICQBot.Models.InlineKeyboard() { Text = "Web", Url = "http://www.google.com" },
             //    new ICQBot.Models.InlineKeyboard() { Text = "Ok", CallbackData = "1" },
             //    new ICQBot.Models.InlineKeyboard() { Text = "Cancel", CallbackData = "2" }
